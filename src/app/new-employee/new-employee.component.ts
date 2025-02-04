3e22a61800b26ab50f549b57edecdfff07ee4928
tjark <tjarksiever75@gmail.com>
2025-02-04 18:34:20 +0100

    New-Employee Seite nach unseren vorstellungen erstellt. Es fehlt das speichern

--- src/app/new-employee/new-employee.component.ts
+++ src/app/new-employee/new-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-employee',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-employee.component.css']
 })
 export class NewEmployeeComponent {
+  // Datenmodell für den Mitarbeiter
+  formData = {
+    firstName: '',
+    lastName: '',
+    phone: '',
+    city: '',
+    street: '',
+    postalCode: ''
+  };
 
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    console.log('Gespeicherte Daten:', this.formData);
+    this.router.navigate(['/']); // Zurück zur Startseite
+  }
+
+  onCancel() {
+    this.router.navigate(['/']); // Zurück zur Startseite
+  }
 }
