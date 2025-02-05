dfa65d70983feb01cebf12b4a438959d25a7ec2c
Lysander <lysander1@schule.bremen.de>
2025-02-05 09:24:31 +0100

    Fügt Überprüfung für alle Felder und Postleitzahl

--- src/app/new-employee/new-employee.component.ts
+++ src/app/new-employee/new-employee.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 import {HttpClient} from "@angular/common/http";
 
 @Component({
@@ -17,8 +17,11 @@ export class NewEmployeeComponent {
     postcode: ''
   };
 
+  showError: Boolean = false;
+  falsePLZ: Boolean = false;
 
-  constructor(private router: Router, private http: HttpClient) {}
+  constructor(private router: Router, private http: HttpClient) {
+  }
 
   onSubmit() {
     const employeeData = {
@@ -30,20 +33,37 @@ export class NewEmployeeComponent {
       postcode: this.formData.postcode
     };
 
-    console.log('Gespeicherte Daten:', employeeData);
-    this.http.post('http://localhost:8089/employees', employeeData)
-      .subscribe({
-        next: (response) => {
-          console.log('Gespeicherte Daten:', response);
-          this.router.navigate(['/']);
-        },
-        error: (error) => {
-          console.error('Fehler beim Speichern des Mitarbeiters:', error)
-        }
-      });
+    if (employeeData.firstName == '' || employeeData.lastName == '' || employeeData.phone == '' || employeeData.city == '' || employeeData.postcode == '' || employeeData.street == '') {
+      this.showError = true;
+
+    } else if (employeeData.postcode.length < 5) {
+      this.falsePLZ = true;
+
+    } else {
+      console.log('Gespeicherte Daten:', employeeData);
+      this.http.post('http://localhost:8089/employees', employeeData)
+        .subscribe({
+          next: (response) => {
+            console.log('Gespeicherte Daten:', response);
+            this.router.navigate(['/']);
+          },
+          error: (error) => {
+            console.error('Fehler beim Speichern des Mitarbeiters:', error)
+          }
+        });
+    }
+
   }
 
   onCancel() {
     this.router.navigate(['/']);
+  }
+
+  falsePLZOff() {
+    this.falsePLZ = false;
+  }
+
+  showErrorOff() {
+    this.showError = false;
   }
 }
